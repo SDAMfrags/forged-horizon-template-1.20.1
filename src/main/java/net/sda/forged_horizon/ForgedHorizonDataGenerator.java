371011dfd17f070882428a7e711042a9e73721e6
SDAMfrags <shadow86693@gmail.com>
2026-06-17 17:53:02 +0530

    Initial commit

---
+++ src/main/java/net/sda/forged_horizon/ForgedHorizonDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sda.forged_horizon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ForgedHorizonDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
